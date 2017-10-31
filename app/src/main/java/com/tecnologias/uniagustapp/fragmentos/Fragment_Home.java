24b2fbef527367861042718cf15046486be10efd
Ivan <ivan.lobelo@uniagustiniana.edu.co>
2017-10-31 15:10:18 -0500

    Se agregan nuevos fragments relacionados al MenuBottom: Noticias, Calendario, PQRS.

--- app/src/main/java/com/tecnologias/uniagustapp/fragmentos/Fragment_Home.java
+++ app/src/main/java/com/tecnologias/uniagustapp/fragmentos/Fragment_Home.java
@@ -7,6 +7,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,14 +29,15 @@ public class Fragment_Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment__home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         final Fragment noticias = new Fragment_Noticias();
+        final Fragment calendario = new Fragment_Calendario();
+        final Fragment pqrs = new Fragment_PQRS();
+        final Fragment preinscrip = new Fragment_Pre_Inscripcion();
+
         /*
-        final Fragment calendario = new Calendario();
         final Fragment ubicacion = new Map_Fragment();
-        final Fragment pqrs = new PQRS();
-        final Fragment preinscrip = new PreInscripcion();
         */
 
         BottomNavigationView navigation = (BottomNavigationView) v.findViewById(R.id.navigation);
@@ -49,14 +51,18 @@ public class Fragment_Home extends Fragment {
 
                 if (item.getItemId() == R.id.noticias) {
                     fragmentTransaction.replace(R.id.content_main, noticias).commit();
+                    ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Noticias");//Agrega titulo a ActionBar
                 } else if (item.getItemId() == R.id.rutas) {
 
                 } else if (item.getItemId() == R.id.cal_aca) {
-
+                    fragmentTransaction.replace(R.id.content_main, calendario).commit();
+                    ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Calendario Académico");//Agrega titulo a ActionBar
                 } else if (item.getItemId() == R.id.pqrs) {
-
+                    fragmentTransaction.replace(R.id.content_main, pqrs).commit();
+                    ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Servicio al Cliente");//Agrega titulo a ActionBar
                 } else if (item.getItemId() == R.id.preinscripcion) {
-
+                    fragmentTransaction.replace(R.id.content_main, preinscrip).commit();
+                    ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Diligenciar Formulario");//Agrega titulo a ActionBar
                 }
                 return true;
             }
