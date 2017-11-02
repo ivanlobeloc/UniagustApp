49bf5d65ac7e79f04e7541ff531c0ff67a3296a2
Ivan <ivan.lobelo@uniagustiniana.edu.co>
2017-11-02 08:25:24 -0500

    bk 16, se realizan las correcciones necesarias para que se muestre correctamente el mapa con el acercamiento dispuesto.

--- app/src/main/java/com/tecnologias/uniagustapp/fragmentos/Fragment_Rutas.java
+++ app/src/main/java/com/tecnologias/uniagustapp/fragmentos/Fragment_Rutas.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -32,25 +30,25 @@ public class Fragment_Rutas extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rutas, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_rutas, container, false);
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //SupportMapFragment mapFragment2 = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
-        //mapFragment2.getMapAsync((OnMapReadyCallback) getActivity());
+        return v;
+    }
 
-        //MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
-        //fragment.getMapAsync(this);
+    @Nullable
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 
     @Override//acercamiento
     public void onMapReady(GoogleMap googleMap) {
+
         LatLng bogota = new LatLng(4.653421, -74.145150);
         googleMap.addMarker(new MarkerOptions().position(bogota)
                 .title("Uniagustiniana"));
